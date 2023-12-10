09e13c780aafe2c9df469523812c295eaf621e80
Zoe Nguyen <znguyen2007@gmail.com>
2023-12-10 21:51:03 +0000

    add all

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+import cors from 'cors';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req,res) =>{
+res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req,res) =>{
+ const commentId= randomBytes(4).toString('hex');
+ const { content }= req.body;
+
+ const comments = commentsByPostId[req.params.id] || [] ;
+ comments.push({id: commentId, content});
+ commentsByPostId[req.params.id] = comments;
+
+ res.status(201).send(comments);
+});
+
+app.listen(4004, () =>{
+    console.log("Listening on port 4004");
+})
---
+++ posts/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+import cors from 'cors';
+
+const app = express();
+
+//
+app.use(bodyParser.json());
+app.use(cors());
+const posts= {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req,res) =>{
+ //generate random id
+ const id= randomBytes(4).toString('hex');
+ const { title } = req.body;
+
+ posts[id] ={
+    id, title
+ };
+ res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () =>{
+    console.log('Listening on port 4000');
+})
